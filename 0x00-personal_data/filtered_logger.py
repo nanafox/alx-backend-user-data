43ca02d3b62152c19a8ed89a226de712d0d5ac9d
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-08-01 23:19:01 +0000

    feat: Add 'get_db' function to setup DB connection

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,9 +4,12 @@
 """This module defines the `filter_datum` function."""
 
 import logging
+import os
 import re
 from typing import List
 
+from mysql.connector import errors
+from mysql.connector.connection import MySQLConnection
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
@@ -60,3 +63,16 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db() -> MySQLConnection:
+    """Return a database connection."""
+    try:
+        return MySQLConnection(
+            user=os.environ.get("PERSONAL_DATA_DB_USERNAME", "root"),
+            password=os.environ.get("PERSONAL_DATA_DB_PASSWORD", ""),
+            host=os.environ.get("PERSONAL_DATA_DB_HOST", "localhost"),
+            database=os.environ.get("PERSONAL_DATA_DB_NAME"),
+        )
+    except errors.ProgrammingError as e:
+        logging.error(f"Database connection failed - {e.msg}")
