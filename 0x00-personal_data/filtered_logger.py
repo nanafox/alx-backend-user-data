e6cb52f8dcd869a617f7848cbef97c8a467f914a
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-08-01 22:19:57 +0000

    feat: Add redacting class

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,8 +3,12 @@
 
 """This module defines the `filter_datum` function."""
 
+import logging
 import re
 from typing import List
+
+
+logger = logging.getLogger(__name__)
 
 
 def filter_datum(
@@ -15,3 +19,27 @@ def filter_datum(
         rf"({'|'.join(map(re.escape, fields))})=([^\s{re.escape(separator)}]*)"
     )
     return re.sub(pattern, rf"\1={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class."""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+    logging.basicConfig(format=FORMAT, level=logging.INFO)
+
+    def __init__(self, fields: List[str]):
+        """Initialize logger."""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filter values coming from log records."""
+        record.msg = filter_datum(
+            fields=self.fields,
+            redaction=self.REDACTION,
+            message=record.msg,
+            separator=self.SEPARATOR,
+        )
+        return super(RedactingFormatter, self).format(record=record)
