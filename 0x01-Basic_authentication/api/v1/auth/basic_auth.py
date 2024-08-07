16f7815be52911deb8ea0f52bb428e35798a10ab
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-08-07 23:17:21 +0000

    feat: Implement a method to extract the auth header info

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,3 +7,16 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """BasicAuth class to manage the API authentication."""
+
+    @staticmethod
+    def extract_base64_authorization_header(authorization_header: str):
+        """Return the value of the Authorization header."""
+        if not authorization_header or not isinstance(
+            authorization_header, str
+        ):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header[6:]
